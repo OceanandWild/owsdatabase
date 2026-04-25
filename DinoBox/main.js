02f43c7eebbabfe711c625ac5103a433eb0e2d24
OceanandWild <oceanandwild@users.noreply.github.com>
2026-04-25 00:20:14 -0300

    release: dinobox 2026.4.25-t0143

---
+++ DinoBox/main.js
@@ -0,0 +1,38 @@
+﻿const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1366,
+    height: 820,
+    minWidth: 1024,
+    minHeight: 680,
+    icon: path.join(__dirname, 'build', 'dinobox.ico'),
+    autoHideMenuBar: true,
+    backgroundColor: '#05070a',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
